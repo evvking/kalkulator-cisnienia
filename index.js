b71749ac8a6d2caed4c91c6e7cb62b128f7dd4c5
PANT Studios <87782409+evvking@users.noreply.github.com>
2021-07-28 15:53:08 +0200

    Changed GUI

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById('credits').addEventListener('click', function() {
+    alert('Ikony: Freepik (od flaticon.com)')
+})
--- pressure.js
+++ pressure.js
@@ -2,9 +2,18 @@ function cPressure(altitude, absolutePressure) {
     return absolutePressure * Math.exp(-0.000125 * altitude)
 }
 
-function main() {
+document.getElementById('altitude').addEventListener('change', function() {
+    if (document.getElementById('abs').value != null) {
+        altitude = document.getElementById('altitude').value
+        absolutePressure = document.getElementById('abs').value
+        pressure = Math.round(cPressure(altitude, absolutePressure))
+        document.getElementById('atm').value = pressure + ' hPa'
+    }
+})
+
+document.getElementById('abs').addEventListener('change', function() {
     altitude = document.getElementById('altitude').value
     absolutePressure = document.getElementById('abs').value
     pressure = Math.round(cPressure(altitude, absolutePressure))
-    document.getElementById('result').textContent = 'Ciśnienie na wysokości ' + altitude + ' m n.p.m: ' + pressure + ' hPa'
-}
+    document.getElementById('atm').value = pressure + ' hPa'
+})
